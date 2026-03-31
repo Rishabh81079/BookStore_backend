4568aa5ed4b8e80f8f294ac66d80e2bd89308ac5
Rishabh <Rishabh94121@gmail.com>
2026-03-31 14:47:51 +0530

    Second commit

--- index.js
+++ index.js
@@ -22,8 +22,11 @@ app.get("/book",(req,res)=>{
 app.get("/book/:id",(req,res)=>{
     const id = parseInt( req.params.id)
     console.log(id);
-    
+
     const booker = bookStore.find(data=> data.id=== id)
+    if(!booker){
+    return res.status(404).send("Book not found")
+}
     res.send(booker)
 })
 
@@ -32,8 +35,60 @@ app.post("/book",(req,res)=>{
     console.log(req.body);
     
     bookStore.push(req.body)
+    res.status(201)
     res.send("Data saved successfully")
 })
+
+
+
+
+app.delete("/book/:id",(req,res)=>{
+    const id = parseInt(req.params.id)
+    console.log(id);
+    
+    const del = bookStore.findIndex(value => value.id === id)
+    bookStore.splice(del,1)
+    res.send("deleted")
+})
+
+
+app.put("/book/:id",(req,res)=>{
+    const id = parseInt(req.params.id)
+    const update = bookStore.find(value => value.id === id)
+    if (!update) {
+    return res.status(404).send("Book not found")
+}
+    const author = req.body.author
+    if(author){
+        update.author = author
+    }
+    const name = req.body.name
+    if(name){
+        update.name= name
+    }
+    res.send("updated")
+})
+
+app.patch("/book/:id", (req, res) => {
+    const id = parseInt(req.params.id)
+
+    const book = bookStore.find(value => value.id === id)
+
+    if (!book) {
+        return res.status(404).send("Book not found")
+    }
+
+    if (req.body.name !== undefined) {
+        book.name = req.body.name
+    }
+
+    if (req.body.author !== undefined) {
+        book.author = req.body.author
+    }
+
+    res.send(book)
+})
+
 
 
 
